2dda22aa1821803298cc1c152ac101a876f2dafd
Felipeeelima <felip.e.e@hotmail.com>
2024-12-06 10:46:38 -0300

    nome tela menu

--- MountainShooter-main/code/Menu.py
+++ MountainShooter-main/code/Menu.py
@@ -24,6 +24,9 @@ class Menu:
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
+            self.menu_text(10, "Felipe Lima da Cunha", C_WHITE, ((WIN_WIDTH / 1.15), 30))
+            self.menu_text(10, "RU: 4306466", C_WHITE, ((WIN_WIDTH / 1.15), 40))
+
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
                     self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
